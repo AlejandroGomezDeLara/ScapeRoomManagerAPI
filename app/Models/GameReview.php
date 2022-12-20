e52ce714367ecbed5be4de88fd7844f7e50a2c3d
Alejandro <alejandro.gomez711@gmail.com>
2022-12-20 10:24:12 +0100

    Imagenes guardadas storage local

--- app/Models/GameReview.php
+++ app/Models/GameReview.php
@@ -4,17 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class GameReview extends Model
 {
     use HasFactory;
-    public $guarded=["id"];
-    
-    public function getImageAttribute($value){
-        if($this->attributes['image'])
-        return url('storage/'.$this->attributes['image']);
+    public $guarded = ["id"];
+
+    public function getImageAttribute($value)
+    {
+        if ($this->attributes["image"]) {
+            try {
+                $image = Storage::disk('local')->get($this->attributes["image"]);
+                return "data:image/jpeg;base64," . base64_encode($image);
+            } catch (\Throwable $th) {
+                return $this->attributes['image'];
+            }
+        } else return $this->attributes['image'];
     }
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
 }
