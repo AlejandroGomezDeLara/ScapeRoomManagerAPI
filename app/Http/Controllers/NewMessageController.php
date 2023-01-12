e59605704ae16b35c3dc46d01c675bf007fafba1
Alejandro <alejandro.gomez711@gmail.com>
2023-01-12 10:04:34 +0100

    Mensajes vistos

--- app/Http/Controllers/NewMessageController.php
+++ app/Http/Controllers/NewMessageController.php
@@ -21,12 +21,12 @@ class NewMessageController extends Controller
         if ($count > 0) {
 
             $last_message = NewMessage::where('user_id', Auth::id())->with('chatMessage')->orderBy('id', 'desc')->first();
-            if(isset($last_message))
+            if (isset($last_message))
                 $last_message->chatMessage->user = User::find($last_message->chatMessage->user_id);
         }
         return response()->json([
             'count' => $count,
-            'last_message' => isset($last_message) ? $last_message :null
+            'last_message' => isset($last_message) ? $last_message : null
         ]);
     }
 
@@ -93,19 +93,19 @@ class NewMessageController extends Controller
      */
     public function destroy($chat_id)
     {
-        
-        $messages=ChatMessage::where('user_id',Auth::id())->where('chat_id',$chat_id)->get();
 
-        foreach($messages as $message){
-            NewMessage::where('chat_message_id',$message->id)->delete();
-            $new_messages=NewMessage::where('chat_id',$chat_id)->count();
-            if($new_messages){
-                $message->readed_at=now();
+        $messages = ChatMessage::where('user_id', Auth::id())->where('chat_id', $chat_id)->get();
+
+        foreach ($messages as $message) {
+            NewMessage::where('chat_message_id', $message->id)->delete();
+            $new_messages = NewMessage::where('chat_id', $chat_id)->where('user_id', '!=', Auth::id())->count();
+            if ($new_messages == 0) {
+                $message->readed_at = now();
             }
         }
 
         return response()->json([
-            "message"=>"Messages readed"
+            "message" => "Messages readed"
         ]);
     }
 }
